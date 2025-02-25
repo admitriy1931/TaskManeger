7f5fcec3f51900b7e0c87a696d65dc78fb85a248
admitriy1931 <82140063+admitriy1931@users.noreply.github.com>
2025-02-25 14:56:55 +0500

    delete h2 db

--- src/main/java/com/taskmanagement/models/User.java
+++ src/main/java/com/taskmanagement/models/User.java
@@ -1,15 +1,13 @@
 package com.taskmanagement.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Setter
 @Getter
-@Entity
+@Document
 public class User {
 
     @Id
--- src/main/java/com/taskmanagement/repositories/TaskRepository.java
+++ src/main/java/com/taskmanagement/repositories/TaskRepository.java
@@ -1,7 +1,7 @@
 package com.taskmanagement.repositories;
 
 import com.taskmanagement.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends MongoRepository<Task, Long> {
 }
